bb2f5d2847e0bb1dfa72a0b3347cb5aa94005094
Henry Wolfrum <hw.wolfrum@web.de>
2026-08-19 02:41:15 +0200

    changed to 10k

--- src/RiskEngine.Simulations/Program.cs
+++ src/RiskEngine.Simulations/Program.cs
@@ -4,7 +4,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // Wir starten für den Anfang mit 10.000 Spielen
-        Arena.RunTournament(numberOfGames: 100_000, playerCount: 4);
+        Arena.RunTournament(numberOfGames: 10_000, playerCount: 4);
     }
 }
